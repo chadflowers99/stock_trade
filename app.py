c559b16a281ed905e7cdbb942d14b575a40df8fa
Chad <chadflowers99@gmail.com>
2026-06-25 14:24:37 -0500

    Update app title to Portfolio

--- app.py
+++ app.py
@@ -541,7 +541,7 @@ def _ledger_display_label(record, index):
 
 # Main App
 st.set_page_config(page_title="Stock Tracker", layout="centered")
-st.title("J-Trade")
+st.title("Portfolio")
 
 # Authentication
 user = auth_ui()
